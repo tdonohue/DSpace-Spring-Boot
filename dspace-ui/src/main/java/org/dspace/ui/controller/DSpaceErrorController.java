d52f43b8b7bd0b2d75c0a6560a1d9845512647c0
Tim Donohue <tdonohue@duraspace.org>
2016-02-29 12:03:37 -0600

    Enhance Error Handling. Also catch internal errors and report

--- dspace-ui/src/main/java/org/dspace/ui/controller/DSpaceErrorController.java
+++ dspace-ui/src/main/java/org/dspace/ui/controller/DSpaceErrorController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.UrlPathHelper;
 
 /**
  * DSpace Error Controller. This extends our DSpaceController so that it
@@ -23,9 +23,25 @@ public class DSpaceErrorController extends DSpaceController
 {
     public static final String DEFAULT_ERROR_VIEW = "error";
 
+    /**
+     * Responds for all Exceptions or RuntimeException
+     * @param model
+     * @param request
+     * @param e
+     * @return 
+     */
     @ExceptionHandler(value = {Exception.class, RuntimeException.class})
     public String defaultErrorHandler(Model model, HttpServletRequest request, Exception e) 
     {
+        // Add exception info to our model
+        // By default, Spring Boot SHOULD set its own ErrorAttributes, but if the
+        // exception is within Spring Boot's processing, this sometimes fails. 
+        // So, our error page also uses the underlying "exception" as a backup 
+        // source for the error message.
+        model.addAttribute("exception", e);
+        model.addAttribute("exception_type", e.getClass().getName());
+        model.addAttribute("exception_path", new UrlPathHelper().getPathWithinApplication(request));
+
         // Just display our default (themed) error page for all exceptions
         return DEFAULT_ERROR_VIEW;
     }
